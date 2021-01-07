a3f0f893bcbb7171a01c7f23872716aaa936e660
Brandy Garland <brandy.garland@sleepnumber.com>
2021-01-06 17:09:34 -0700

    cleanup

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_ENV, $INPUT_RAILS_ENV, $INPUT_CI"
-
-sh -c "echo wkspce is $GITHUB_WORKSPACE"
-
-
 chmod +x $GITHUB_WORKSPACE/test.sh	
 sh -c $GITHUB_WORKSPACE/test.sh	
-
-
-sh -c "echo Im done!"
