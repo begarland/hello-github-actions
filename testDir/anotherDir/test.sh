c70c1b5d6479a731ae140be511b977111db54bc4
Brandy Garland <brandy.garland@sleepnumber.com>
2021-01-11 14:23:24 -0700

    try with chrome drive

--- testDir/anotherDir/test.sh
+++ testDir/anotherDir/test.sh
@@ -4,9 +4,4 @@ pwd
 
 sh -c "echo Hello world, Im in another dir!" 
 
-cd $GITHUB_WORKSPACE/testDir/anotherDir
-
-bundle exec cucumber --publish
-
-pwd
 
--- watir-action/entrypoint.sh
+++ watir-action/entrypoint.sh
@@ -10,8 +10,17 @@ pwd
 
 gem install bundler
 
+CHROME_VERSION="$(google-chrome --version | sed -r 's/[^0-9]+([0-9]+\.[0-9]+\.[0-9]+).*/\1/g')"
+CHROMEDRIVER_VERSION_URL="https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION"
+CHROMEDRIVER_VERSION=$(curl -s "$CHROMEDRIVER_VERSION_URL")
+
+bin/bundle exec chromedriver-update "$CHROMEDRIVER_VERSION"
+
 bundle config https://gems.weblinc.com
 bundle install --deployment
+
+
+
 
 chmod +x $GITHUB_WORKSPACE/testDir/anotherDir/test.sh	
 sh -c $GITHUB_WORKSPACE/testDir/anotherDir/test.sh	
