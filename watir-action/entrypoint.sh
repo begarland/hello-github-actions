a9ad9da99a1434623335d87eb1f18fd9790b3c99
Brandy Garland <brandy.garland@sleepnumber.com>
2021-01-11 08:43:42 -0700

    try in sh

--- watir-action/entrypoint.sh
+++ watir-action/entrypoint.sh
@@ -8,8 +8,10 @@ cd $GITHUB_WORKSPACE/testDir/anotherDir
 
 pwd
 
+echo bundle config https://gems.weblinc.com
+echo bundle install --deployment
+
 chmod +x $GITHUB_WORKSPACE/testDir/anotherDir/test.sh	
 sh -c $GITHUB_WORKSPACE/testDir/anotherDir/test.sh	
-
 
 echo "done!"
