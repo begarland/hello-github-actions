820d5c33be487ffd215b739aed8ae92f7edde359
Brandy Garland <brandy.garland@sleepnumber.com>
2021-01-11 08:53:41 -0700

    another one

--- watir-action/entrypoint.sh
+++ watir-action/entrypoint.sh
@@ -10,8 +10,8 @@ pwd
 
 gem install bundler
 
-bundle exec config https://gems.weblinc.com
-bundle exec install --deployment
+bundle config https://gems.weblinc.com
+bundle install --deployment
 
 chmod +x $GITHUB_WORKSPACE/testDir/anotherDir/test.sh	
 sh -c $GITHUB_WORKSPACE/testDir/anotherDir/test.sh	
