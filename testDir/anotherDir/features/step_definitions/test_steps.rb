6554d5a01ea7760146d5dd573c04332e4d94a5ad
Brandy Garland <brandy.garland@sleepnumber.com>
2021-01-11 19:26:09 -0700

    try with only headless an sandbox

--- testDir/anotherDir/features/step_definitions/test_steps.rb
+++ testDir/anotherDir/features/step_definitions/test_steps.rb
@@ -8,9 +8,9 @@ When ('I am testing') do
     chromeOptions = Selenium::WebDriver::Chrome::Options.new
     chromeOptions.add_argument("--no-sandbox") 
     
-    chromeOptions.add_argument("--remote-debugging-port=9222")  # this
+    # chromeOptions.add_argument("--remote-debugging-port=9222")  # this
     
-    chromeOptions.add_argument("--disable-dev-shm-using") 
+    # chromeOptions.add_argument("--disable-dev-shm-using") 
     chromeOptions.add_argument("--headless") 
 
     b = Selenium::WebDriver.for(:chrome, options: chromeOptions)
