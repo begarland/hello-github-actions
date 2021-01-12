9b15f3121621f62fcbec4fdec4755e4e65692dfc
Brandy Garland <brandy.garland@sleepnumber.com>
2021-01-11 19:42:00 -0700

    try again

--- testDir/anotherDir/features/step_definitions/test_steps.rb
+++ testDir/anotherDir/features/step_definitions/test_steps.rb
@@ -7,6 +7,7 @@ When ('I am testing') do
     b = Watir::Browser.new 'chrome'.to_sym,
                        switches: ['--ignore-certificate-errors'],
                        headless: ENV['CI'].present?
+                       nosandbox: [true]
     b.goto("https://google.com/") 
     b.quit()
 
