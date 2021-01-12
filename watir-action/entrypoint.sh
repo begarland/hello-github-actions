4cf5c9b4109c5c7f04e42803784174f36bbe659d
Brandy Garland <brandy.garland@sleepnumber.com>
2021-01-11 21:04:16 -0700

    remove the extra update

--- watir-action/entrypoint.sh
+++ watir-action/entrypoint.sh
@@ -13,7 +13,7 @@ apt-get install -y curl unzip xvfb libxi6 libgconf-2-4 gnupg2
 
 curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
 echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
-apt-get -y update
+# apt-get -y update
 apt-get -y install google-chrome-stable
 
 wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
