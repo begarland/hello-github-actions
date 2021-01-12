6a1d833c3d2bded230a716d957e366e06d22d068
Brandy Garland <brandy.garland@sleepnumber.com>
2021-01-11 20:49:50 -0700

    remove curl

--- watir-action/entrypoint.sh
+++ watir-action/entrypoint.sh
@@ -9,10 +9,8 @@ cd $GITHUB_WORKSPACE/testDir/anotherDir
 pwd
 
 apt-get update
-apt-get install -y curl unzip xvfb libxi6 libgconf-2-4 gnupg2
+apt-get install -y unzip xvfb libxi6 libgconf-2-4 gnupg2
 
-curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 apt-get -y update
 apt-get -y install google-chrome-stable
 
