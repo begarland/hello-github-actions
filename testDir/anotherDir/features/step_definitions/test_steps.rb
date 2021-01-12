afedf84ff2f41af2fd03fbe53c165243564c8554
Brandy Garland <brandy.garland@sleepnumber.com>
2021-01-11 18:45:55 -0700

    test

--- testDir/anotherDir/features/step_definitions/test_steps.rb
+++ testDir/anotherDir/features/step_definitions/test_steps.rb
@@ -4,7 +4,6 @@ When ('I am testing') do
 
     login = "test"
     chromeOptions = Selenium::WebDriver::Chrome::Options.new
-    chromeOptions.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) 
     chromeOptions.add_argument("--no-sandbox") 
     chromeOptions.add_argument("--disable-setuid-sandbox") 
     
@@ -17,7 +16,7 @@ When ('I am testing') do
     chromeOptions.add_argument("disable-infobars") 
     chromeOptions.add_argument("--headless") 
     chromeOptions.add_argument(r"user-data-dir=.\cookies\\" + login) 
-    b = webdriver.Chrome(chrome_options=chromeOptions) 
+    b = webdriver.Options(chrome_options=chromeOptions) 
     b.get("https://google.com/") 
     b.quit()
 
