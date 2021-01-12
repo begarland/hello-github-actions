b5884d4bdc0b73f5c3c7f6db8a283f7a7b26e089
Brandy Garland <brandy.garland@sleepnumber.com>
2021-01-11 18:20:11 -0700

    try with adding options

--- testDir/anotherDir/features/step_definitions/test_steps.rb
+++ testDir/anotherDir/features/step_definitions/test_steps.rb
@@ -2,6 +2,10 @@ require "selenium-webdriver"
 
 When ('I am testing') do
     browser = Selenium::WebDriver.for:chrome
+    browser.add_argument('--disable-extensions')
+    browser.add_argument('--headless')
+    browser.add_argument('--disable-gpu')
+    browser.add_argument('--no-sandbox')
     browser.get('http://www.google.com')
     log('testing')
     true
