9c11dead4503773bd31ca9caa2281db8089fff72
Brandy Garland <brandy.e.garland@gmail.com>
2021-01-14 12:23:51 -0700

    use double quotes

--- buildbot-billy-action/entrypoint.sh
+++ buildbot-billy-action/entrypoint.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get install -y curl
 
-curl --location --request POST 'https://slack.com/api/chat.postMessage' \
+curl --location --request POST "https://slack.com/api/chat.postMessage" \
 --header 'Content-Type: application/json' \
 --header `Authorization: Bearer $INPUT_BEARER` \
 --data-raw '{
