35a75133739f0b2622941c0447c487c1d7e65663
Brandy Garland <brandy.e.garland@gmail.com>
2021-01-14 12:45:27 -0700

    puse env vars

--- buildbot-billy-action/entrypoint.sh
+++ buildbot-billy-action/entrypoint.sh
@@ -3,10 +3,13 @@
 apt-get update
 apt-get install -y curl
 
+
+AUTH=`Authorization: Bearer $INPUT_BEARER`
+
 curl --location --request POST "https://slack.com/api/chat.postMessage" \
---header "Content-Type: application/json" \
---header "Authorization: Bearer " + $INPUT_BEARER \
---data-raw "{
+--header "Content-Type: application/json' \
+--header $AUTH \
+--data-raw '{
     "channel": "U0149HC3PFV",
     "text": "hello world"
-}"
+}'
