656171eb1bfd2e8f05988b682cdb061f5a5157db
Brandy Garland <brandy.e.garland@gmail.com>
2021-01-14 14:36:50 -0700

    try agaqin

--- buildbot-billy-action/entrypoint.sh
+++ buildbot-billy-action/entrypoint.sh
@@ -6,9 +6,9 @@ apt-get install -y curl
 curl --location --request POST "https://slack.com/api/chat.postMessage" \
 --header "Content-Type: application/json" \
 --header "Authorization: Bearer $INPUT_BEARER" \
---data-raw "{"
-    "channel": "U0149HC3PFV",
+--data-raw '{
+    "channel": '"$INPUT_CHANNEL"',
     "text": "hello world"
-"}"
+}'
 
 echo "message"
