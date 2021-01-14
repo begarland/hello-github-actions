bd6377dd1d212e8a671fc5ddb7d79080f28364ad
Brandy Garland <brandy.e.garland@gmail.com>
2021-01-14 13:29:43 -0700

    try escaping colons

--- buildbot-billy-action/entrypoint.sh
+++ buildbot-billy-action/entrypoint.sh
@@ -3,10 +3,9 @@
 apt-get update
 apt-get install -y curl
 
-
 curl --location --request POST "https://slack.com/api/chat.postMessage" \
---header "Content-Type: application/json" \
---header "Authorization: Bearer $($INPUT_BEARER)" \
+--header "Content-Type\: application/json" \
+--header "Authorization\: Bearer $($INPUT_BEARER)" \
 --data-raw '{
     "channel": "U0149HC3PFV",
     "text": "hello world"
