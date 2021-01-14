6d968af5238925719a00358b92ec7bfed2ba6945
Brandy Garland <brandy.e.garland@gmail.com>
2021-01-14 14:27:45 -0700

    try single quotes

--- buildbot-billy-action/entrypoint.sh
+++ buildbot-billy-action/entrypoint.sh
@@ -7,8 +7,8 @@ curl --location --request POST "https://slack.com/api/chat.postMessage" \
 --header "Content-Type: application/json" \
 --header "Authorization: Bearer $INPUT_BEARER" \
 --data-raw "{
-    "channel": "U0149HC3PFV",
-    "text": "hello world"
+    'channel': "U0149HC3PFV",
+    'text': "hello world"
 }"
 
 echo "message"
