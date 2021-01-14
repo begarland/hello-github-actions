a283fd649f92991760f773583283ca4d4dc7529e
Brandy Garland <brandy.e.garland@gmail.com>
2021-01-14 12:46:42 -0700

    was it a typo this whole time

--- buildbot-billy-action/entrypoint.sh
+++ buildbot-billy-action/entrypoint.sh
@@ -7,7 +7,7 @@ apt-get install -y curl
 AUTH=`Authorization: Bearer $INPUT_BEARER`
 
 curl --location --request POST "https://slack.com/api/chat.postMessage" \
---header "Content-Type: application/json' \
+--header "Content-Type: application/json" \
 --header $AUTH \
 --data-raw '{
     "channel": "U0149HC3PFV",
