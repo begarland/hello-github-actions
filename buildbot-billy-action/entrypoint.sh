73aad0f6b61823e223ecada089fb545d0867f3fd
Brandy Garland <brandy.e.garland@gmail.com>
2021-01-14 14:39:02 -0700

    try again

--- buildbot-billy-action/entrypoint.sh
+++ buildbot-billy-action/entrypoint.sh
@@ -3,12 +3,15 @@
 apt-get update
 apt-get install -y curl
 
+
+DATA="{
+    'channel': '$INPUT_CHANNEL', 
+    'text': 'hello world'
+}"
+
 curl --location --request POST "https://slack.com/api/chat.postMessage" \
 --header "Content-Type: application/json" \
 --header "Authorization: Bearer $INPUT_BEARER" \
---data-raw '{
-    "channel": '"$INPUT_CHANNEL"',
-    "text": "hello world"
-}'
+--data-raw $DATA
 
 echo "message"
