eec09add22b08125f6ed457a02f72887d2b1a6cb
Brandy Garland <brandy.e.garland@gmail.com>
2021-01-14 13:51:12 -0700

    add message and channel

--- buildbot-billy-action/entrypoint.sh
+++ buildbot-billy-action/entrypoint.sh
@@ -6,9 +6,9 @@ apt-get install -y curl
 curl --location --request POST "https://slack.com/api/chat.postMessage" \
 --header "Content-Type: application/json" \
 --header "Authorization: Bearer $INPUT_BEARER" \
---data-raw '{
-    "channel": "U0149HC3PFV",
-    "text": "hello world :party-wizard:"
-}'
+--data-raw "{
+    "channel": $INPUT_CHANNEL
+    "text": $INPUT_MESSAGE
+}"
 
 echo "message sent"
