a83f4cfd3592bc7b6309a516ec5eaf72b68fc2c0
Brandy Garland <brandy.e.garland@gmail.com>
2021-01-14 20:04:47 -0700

    does jq run?

--- buildbot-billy-action/entrypoint.sh
+++ buildbot-billy-action/entrypoint.sh
@@ -9,7 +9,7 @@ echo $INPUT_MESSAGE
 CHANNEL='"channel": ''"'$INPUT_CHANNEL'",'
 TEXT='"text": ''"'$INPUT_MESSAGE'"'
 
-MESSAGES= $(echo 'messages.json' | jq -r -c '.' )
+MESSAGES=$('"status":{"success":"has completed successfully. :mav-sunnys:","failure":"has failed. :party-try-not-to-cry:","cancelled":"has been cancelled. :suspicious:"}' | jq '.status')
 
 echo $MESSAGES
 echo '{"fruit":{"name":"apple","color":"green","price":1.20}}' | jq '.'
