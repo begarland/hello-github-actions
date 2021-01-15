7f113dd2a2a6d9c37f9e7d961f644871f0788d59
Brandy Garland <brandy.e.garland@gmail.com>
2021-01-15 14:20:28 -0700

    try with new mesage

--- buildbot-billy-action/entrypoint.sh
+++ buildbot-billy-action/entrypoint.sh
@@ -16,7 +16,7 @@ USERS=$(cat "$USERS_FILE")
 
 
 CHANNEL_ID=`echo $USERS | jq -r ".$INPUT_DEVELOPER.slack_id"`
-STATUS_MESSAGE=`echo $MESSAGES | jq -r ".job.status.$INPUT_STATUS`
+STATUS_MESSAGE=`echo $MESSAGES | jq -r ".job.status.$INPUT_STATUS"`
 
 [[ ! -z "$INPUT_CHANNEL" ]] && CHANNEL_ID=`echo $CHANNELS | jq ".$INPUT_CHANNEL"` || CHANNEL_ID=`echo $USERS | jq ".$INPUT_DEVELOPER.slack_id"`
 
